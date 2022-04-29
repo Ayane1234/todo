6c740ca3768966133be26fe97966431f19d1a6e4
Ayane1234 <ayane0703@gmail.com>
2022-04-29 21:12:06 +0900

    アイコンのインポート、スタイルの指定

--- src/screens/TodoList.js
+++ src/screens/TodoList.js
@@ -1,4 +1,11 @@
 import React, { useState } from "react";
+import { Grid } from "@mui/material";
+import { TextField } from "@mui/material";
+import { Stack } from "@mui/material";
+import { Paper } from "@mui/material";
+
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 export const TodoList = () => {
   const [todos, setTodos] = useState([]);
@@ -33,28 +40,59 @@ export const TodoList = () => {
     console.log("todolist2:", todolist2);
     console.log("setTodos(todolist2)後のtodos:", todos);
   };
+
   return (
     <div>
-      <h1>ToDoList</h1>
-      <div>
-        {/* <form> */}
-        <input
+      <Stack container spacing={3} alignItems="center">
+        <Grid item>
+          <TextField
+            id="standard-basic"
+            label="input your todo"
+            variant="standard"
+            onChange={(event) => handleForm(event)}
+            value={input}
+            style={styles.TextField}
+          />
+          {/* <input
           onChange={(event) => handleForm(event)}
           type="text"
           value={input}
           placeholder="input your todo!"
-        ></input>
-        <button onClick={() => handleSubmit()}>クリック</button>
-        {/* </form> */}
-      </div>
-      {todos.map((todo, index) => {
-        return (
-          <p index={index}>
-            {todo}
-            <button onClick={() => handleRemoveTodo(index)}>削除</button>
-          </p>
-        );
-      })}
+        ></input> */}
+          <AddCircleIcon onClick={() => handleSubmit()} />
+          {/* <button onClick={() => handleSubmit()}>クリック</button> */}
+        </Grid>
+
+        <Grid item>
+          {todos.map((todo, index) => {
+            return (
+              <Paper
+                elevation={10}
+                variant="outlined"
+                style={styles.todo}
+                index={index}
+              >
+                {todo}
+                <DeleteIcon
+                  onClick={() => handleRemoveTodo(index)}
+                  style={{ color: "gray" }}
+                />
+                {/* <button onClick={() => handleRemoveTodo(index)}>削除</button> */}
+              </Paper>
+            );
+          })}
+        </Grid>
+      </Stack>
     </div>
   );
+};
+const styles = {
+  todo: {
+    // border: "1px solid gray",
+    margin: "12px",
+    width: "300px",
+    backgroundColor: "#AEC4E5",
+    boxShadow: "4px 4px #4072B3",
+    padding: "4px 2px",
+  },
 };
